8901c30d594d660788c55b5b6b5965360b503362
fk-2code <firdewskemalbekri@gmail.com>
2026-04-06 13:12:17 +0300

    Add files via upload
    Checks whether the given number is prime or not

---
+++ prime.py
@@ -0,0 +1,14 @@
+def is_prime(n):
+    if n <=1:
+        return False
+    for i in range(2,int(n**0.5) +1):
+        if n%i==0:
+            return False
+    return True
+num=int(input("enter a number:"))
+if is_prime(num):
+    print("it is prime")
+else:
+    print("it is not prime")
+
+
